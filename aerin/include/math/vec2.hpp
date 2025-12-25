6e6f928609b5f6941063a40100f93159981b23c4
Kacper Gajko <kacper.gajko1@icloud.com>
2025-12-25 19:36:12 +0100

    Vector 2 - + *

---
+++ aerin/include/math/vec2.hpp
@@ -0,0 +1,36 @@
+namespace Aerin {
+  struct Vec2 {
+    float x, y;
+
+    Vec2(float x, float y) : x(x), y(y) {};
+
+    Vec2 Add(const Vec2 &other) const {
+      return Vec2(x + other.x, y + other.y);
+    };
+
+    Vec2 Subtract(const Vec2 &other) const {
+      return Vec2(x - other.x, y - other.y);
+    };
+
+    float Dot(const Vec2 &other) const {
+      return float((x * other.x) + (y * other.y));
+    };
+
+    Vec2 Cross(const float &other) const {
+      return Vec2(x * other, y * other);
+    };
+
+    Vec2 operator+(const Vec2 &other) const {
+      return Add(other);
+    };
+
+    Vec2 operator-(const Vec2 &other) const {
+      return Subtract(other);
+    };
+
+    float operator*(const Vec2 &other) const {
+      return Dot(other);
+    };
+  };
+
+} // namespace Aerin
