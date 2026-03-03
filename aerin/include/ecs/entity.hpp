e8280822f1f61a664015f40db12b89aaa36458b1
Kacper Gajko <kacper.gajko1@icloud.com>
2026-03-03 08:27:02 +0100

    ecs without systems yet

---
+++ aerin/include/ecs/entity.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Aerin {
+
+  using EntityID = uint32_t;
+
+  class EntityManager;
+
+  class Entity {
+  public:
+    EntityID id;
+    EntityManager *manager;
+
+    Entity(EntityID id, EntityManager *manager) : id(id), manager(manager) {};
+  };
+} // namespace Aerin
--- sandbox/app.cpp
+++ sandbox/app.cpp
@@ -1,4 +1,6 @@
 #include "core/app.hpp"
+#include "ecs/entity-manager.hpp"
+#include "ecs/entity.hpp"
 
 int main() {
   Aerin::AppSpecs specs;
@@ -10,5 +12,14 @@ int main() {
   specs.windowSpecs.vsync = false;
 
   Aerin::App app(specs);
+  Aerin::EntityManager entityManager;
+  Aerin::Entity id1 = entityManager.CreateEntity();
+  Aerin::Entity id2 = entityManager.CreateEntity();
+  Aerin::Entity id3 = entityManager.CreateEntity();
+  Aerin::Entity id4 = entityManager.CreateEntity();
+  Aerin::Entity id5 = entityManager.CreateEntity();
+  entityManager.DestroyEntity(id3);
+
+  Aerin::Entity id6 = entityManager.CreateEntity();
   app.Start();
 };
